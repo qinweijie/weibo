a1ea0c074b1211cbc32be27f81dcaab878de14a8
qinweijie <843579596@qq.com>
2019-05-16 10:29:04 +0800

    命命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,14 @@
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
   </body>
 </html>
